cd248279574f6c3cdcade3a1420fbaceb512197b
luav <luardev@gmail.com>
2017-10-04 16:06:07 +0200

    Minor refactoring

--- src/info/exascale/statix/CosineSimilarityMatix.java
+++ src/info/exascale/statix/CosineSimilarityMatix.java
@@ -10,6 +10,29 @@ import java.nio.file.Files;
 import java.util.*;
 
 
+class InstanceProperties {
+	public int id;  //!< Instance (subject) id
+	public boolean isTyped = false;  //!< The instance is typped
+	
+	// Note: the set will be empty if the instance has only the #type properties, but this is a very rare usecase
+	public TreeSet<String> properties = new TreeSet<String>();
+	
+	 
+	public InstanceProperties(int id, String property)
+	{
+		if(property == null || property.isEmpty())
+			throw new IllegalArgumentException("The property is empty for id: " + id);
+		
+		this.id = id;
+		properties.add(property);
+	}
+	
+	public InstanceProperties(int id)
+	{
+		this.id=id;
+	}
+}
+
 public class CosineSimilarityMatix {
 	public static final String  typeProperty = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>";
 	public HashMap<String, Float>  propsWeights = null;  // Used in similarity evaluation
